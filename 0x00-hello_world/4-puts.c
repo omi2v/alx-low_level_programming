ab2f7dad54d76d5b2424819dd98a16e6801d7876
omi2v <omibedewi@gmail.com>
2022-06-02 12:02:27 -0700

    fs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/*
- * int main - structural
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,\n"
  * Return: 0 end of program
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-return (0);
-}
+   puts("Programming is like building a multilingual puzzle");
+		return (0); }
