c7d635911191ff611e1a6d0df8bd183a29ae9b78
omi2v <omibedewi@gmail.com>
2022-06-02 11:30:47 -0700

    fe

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art,\n" 
+ * main - prints "with proper grammar, but the outcome is a piece of art,\n"
+ * Return: 0 end of program  
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-/**
- * Return: 0
- */
 	 	return (0); }
