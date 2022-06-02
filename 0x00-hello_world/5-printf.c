4c1a6b0291c5c4dc715d683f1c40f93cab31aff7
omi2v <omibedewi@gmail.com>
2022-06-02 07:14:43 -0700

    de

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
+#!/bin/bash
 #include <stdio.h>
 /* this prototype has to be declared in a header file */
-int main( void )
+int main(void)
 {
-	printf( "with proper grammar, but the outcome is a piece of art,\n" );
-	 	return( 0 );
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	 	return(0);
 }
