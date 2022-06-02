0b864919c2723f6a924da7a6e1c76f1c631cbc4a
omi2v <omibedewi@gmail.com>
2022-06-02 11:12:34 -0700

    be

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*this is comment for main*/
+/**
+ * this is comment for main
+ * this is for main
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
