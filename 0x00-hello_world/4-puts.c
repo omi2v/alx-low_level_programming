920ccc7ef905f520224981421f69086f03b7c882
omi2v <omibedewi@gmail.com>
2022-06-02 07:18:02 -0700

    for

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,5 @@
-#!/bin/bash
 #include <stdio.h>
 int main()
 {
-
-	puts("Programming is like building a multilingual puzzle\n");
-	
-	getchar();
-	return 0;
-
-}
+puts("Programming is like building a multilingual puzzle\n");
+		return 0;}
