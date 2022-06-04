cb9b59a2046692cc1841e66ffd33786c765084ab
omi2v <omibedewi@gmail.com>
2022-06-04 02:14:34 -0700

    sds

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ if (n % 10 == 0)
 	printf("%i n", n);
 	printf("%i n and is 0\n", n);
 }
-if (n % 10 < 6)
+if ((n % 10) < 6)
 {
 	printf("%i n", n);
 	printf("%i n and is less than 6 and not 0\n", n);
