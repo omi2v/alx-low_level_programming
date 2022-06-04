3abe785ba1cfbfd5a07d2f9e68d935efd7891f70
omi2v <omibedewi@gmail.com>
2022-06-04 04:06:17 -0700

    sds

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ if (n % 10 == 0)
 {
 	printf("Last digit of %d is %d and is 0\n", n, n % 10);
 }
-if (n % 10 < 6)
+if (n % 10 < 6 && !0)
 {
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 }
