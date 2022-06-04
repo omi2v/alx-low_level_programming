da011deaccb780b44d84cf3058f5e34d674c80aa
omi2v <omibedewi@gmail.com>
2022-06-04 04:20:18 -0700

    sd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: get a random number and print the number
+ * if it is positive, negative or zero
+ * Return: 0
+ */
+int main(void)
+{
+     int ch;
+     for (ch = 'a'; ch <= 'z'; ch++)
+	    putchar (ch);
+    putchar ('\n'); 
+        return (0);
+}
