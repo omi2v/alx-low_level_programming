3d89d48f5b49d0bf1f43ba4764f58640300291be
omi2v <omibedewi@gmail.com>
2022-06-04 13:06:39 -0700

    ds

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,9 +9,9 @@
  */
 int main(void)
 {
-	int ch;
-	for (ch = 'a'; ch <= 'z'; ++ch)
-		putchar (ch);
-	putchar ('\n');
-	return (0);
+int ch;
+for (ch = 'a'; ch <= 'z'; ++ch)
+putchar (ch);
+putchar ('\n');
+return (0);
 }
