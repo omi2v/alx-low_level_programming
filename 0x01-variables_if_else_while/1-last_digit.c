e1d27141f5d0e9dc1fc9088089be0183a6efc6d3
omi2v <omibedewi@gmail.com>
2022-06-04 02:11:03 -0700

    fdf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,17 +17,17 @@ int main(void)
 printf("%i Last digit of\n", n);
 printf("%i n\n", n);
 printf("%i is\n", n);
-	if (n% > 5)
+	if (n % 10 > 5)
 	{
 		printf("%i n", n);
 		printf("%i and is greater than 5\n", n);
 	}
-if (n == 0)
+if (n % 10 == 0)
 {
 	printf("%i n", n);
 	printf("%i n and is 0\n", n);
 }
-if (n < 6)
+if (n % 10 < 6)
 {
 	printf("%i n", n);
 	printf("%i n and is less than 6 and not 0\n", n);
