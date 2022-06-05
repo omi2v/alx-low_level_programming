c4c4b1ecdf8b7c22963ab6d7f031b62211a9527b
omi2v <omibedewi@gmail.com>
2022-06-05 04:37:57 -0700

    fhs

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: get a random number and print the number
+ * for loop
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+for (i = 48; i <= 55; i++)
+{
+for (j = 49; j <= 56; j++)
+{
+for (k = 50; k <= 57; k++)
+{
+if (i < j && i < k && j < k)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if ((i < 55) || (j < 56) || (k<57))
+{
+putchar(44);
+putchar(32)
+}
+}
+}
+}
+}
+putchar(10);
+return (0);
+}
