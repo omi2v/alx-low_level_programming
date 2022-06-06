b20e14be514f523c9a2ec9dba26afacd114fadbe
omi2v <omibedewi@gmail.com>
2022-06-06 03:20:42 -0700

    dhs

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: get a random number and print the number
+ * for loop
+ * Return: 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 'z'; ch >= 'a'; ++ch)
+putchar (ch);
+putchar ('\n');
+return (0);
+}
