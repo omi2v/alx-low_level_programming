a35678aa142d16b78c391e0ec198a700366b2d35
omi2v <omibedewi@gmail.com>
2022-06-06 03:28:59 -0700

    six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: get a random number and print the number
+ * if it is positive, negative or zero
+ * Return: 0
+ */
+int main(void)
+{
+int n = 0;
+        while (n < 10)
+        {
+       putchar ("%i", n);
+        n++;
+        }
+	putchar ('\n');
+        return (0);
+}
+
