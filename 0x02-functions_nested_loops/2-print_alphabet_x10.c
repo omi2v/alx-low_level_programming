2644a605b15c9791aed0c78ebb8910253368ecc2
omi2v <omibedewi@gmail.com>
2022-06-07 13:35:04 -0700

    dfds

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
 
 void print_alphabet_x10(void)
 {
-  char alpha;
-  int x = 0;
-    while (x < 10)
-    {
-    alpha = 'a';
-      while (alpha <= 'z')
-      {
-        _putchar(alpha);
-        alpha++;
-      }
-      _putchar('\n');
-      x++;
-    }
+char alpha;
+int x = 0;
+while (x < 10)
+{
+alpha = 'a';
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+x++;
+}
 }
