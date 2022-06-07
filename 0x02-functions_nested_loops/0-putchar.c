211ce269a08e0f7cc3247d1333fb727bb54f4d6d
omi2v <omibedewi@gmail.com>
2022-06-07 03:19:43 -0700

    dsh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,9 @@
  * main - prints "with proper grammar, but the outcome is a piece of art,\n"
  * Return: 0 end of program
  */
-int main(void)
+int main (void)
 {
-printf("_putchar\n");
+putchar("_putchar\n");
 return (0);
 }
 
