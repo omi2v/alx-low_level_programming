4fd6d27b868d65e6c8f181f6f1964ca65906a7d8
omi2v <omibedewi@gmail.com>
2022-06-07 02:40:06 -0700

    sdsvgcsd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,\n"
+ * Return: 0 end of program
+ */
+int main(void)
+{
+puts("_putchar");
+return (0); }
+
