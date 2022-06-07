12ebc6b6f71cc4ab59a062b6f8a43987d530c612
omi2v <omibedewi@gmail.com>
2022-06-07 08:36:23 -0700

    gdss

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: get a random number and print the number
+ * for loop
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ++ch)
+putchar (ch);
+putchar ('\n');
+}
+int main(void)
+{
+print_alphabet();
+return(0);
+}
+
