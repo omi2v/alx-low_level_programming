57a883ab00d08961cb99cb1c2f81aa6dd08f25da
omi2v <omibedewi@gmail.com>
2022-06-08 02:27:24 -0700

    DS

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+*print_sign - returns 1 if lowercase alphabet and 0 otherwise
+*@c: takes in a character
+*Return: 0 for uppercase, 1 for lowercase
+*/
 int print_sign(int n)
 {
 if (n > 0)
@@ -16,4 +21,4 @@ if(n < 0)
 	_putchar('-');
 	return (-1);
 }
-
+}
