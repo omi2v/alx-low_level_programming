d31841947318b1a33e429c456ceebfbd2e7e23ee
omi2v <omibedewi@gmail.com>
2022-06-08 02:03:40 -0700

    sds

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_islower - returns 1 if lowercase alphabet and 0 otherwise
+*_isalpha - returns 1 if lowercase alphabet and 0 otherwise
 *@c: takes in a character
 *Return: 0 for uppercase, 1 for lowercase
 */
