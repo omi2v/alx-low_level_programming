1c6664be810eff728405f364b9e19bf5e6f829c6
omi2v <omibedewi@gmail.com>
2022-06-08 06:36:53 -0700

    df

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_abs - prints absolute value of a number
+*@: takes integer
+*Return: value of last digit
+*/
+int _abs(int)
+{
+        int c;
+abs(c);
+_putchar('c');
+        return (c);
+}
+
