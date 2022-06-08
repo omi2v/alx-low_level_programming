e6d921747e32272a2234bdbb364f7c5e4caa77f6
omi2v <omibedewi@gmail.com>
2022-06-08 08:55:42 -0700

    10

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_times_table - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+
+void print_times_table(int n)
+{
+	int i, j, res;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				res = (i * j);
+				if (res <= 99)
+				{
+					_putchar(' ');
+				}
+				if (res <= 9)
+				{
+					_putchar(' ');
+				}
+				if (res >= 100)
+				{
+					_putchar((res / 100) + '0');
+					_putchar(((res / 10)) % 10 + '0');
+				}
+				else if (res <= 99 && res >= 10)
+				{
+					_putchar((res / 10) + '0');
+				}
+				_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
