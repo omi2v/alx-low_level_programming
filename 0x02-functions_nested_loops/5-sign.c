58c35e0a233de6bb5ebd28f555520982c297d58f
omi2v <omibedewi@gmail.com>
2022-06-08 01:01:55 -0700

    ds

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int print_sign(int n);
+{
+if (n > 0)
+{
+_putchar(+);
+return (1);
+}
+if (n == 0)
+{
+_putchar(0);
+return (0);
+}
+if (n < 0)
+{
+	_putchar(-);
+	return (-1);
+}
+}
