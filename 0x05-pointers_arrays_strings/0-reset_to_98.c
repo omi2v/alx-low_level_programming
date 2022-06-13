9a5c38bda139af7fa908eb11b5ccb82e5f3c8326
omi2v <omibedewi@gmail.com>
2022-06-13 02:24:33 -0700

    poiter

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*
- *reset_to_98 - description for interger
- *@*n: n takes interger
+ * reset_to_98 - description for interger
+ * @*n: n takes interger
  */
 void reset_to_98(int *n)
 {
