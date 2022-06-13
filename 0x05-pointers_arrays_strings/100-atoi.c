7766637a035f2a2f1bd0ab36018f60ad8b017d72
omi2v <omibedewi@gmail.com>
2022-06-13 14:41:05 -0700

    convert

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,10 +10,10 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int total = 0;
 	char null_flag = 0;
-	while (*s)
+while (*s)
 	{
-		if (*s == '-')
-	sign *=-1;
+if (*s == '-')
+	sign *= -1;
 		if (*s >= '0' && *s <= '9')
 		{
 			null_flag = 1;
