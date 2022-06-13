7b0e74348e95dd119d924ec204030217f2874cc1
omi2v <omibedewi@gmail.com>
2022-06-13 02:28:34 -0700

    pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * reset_to_98 - this function assign interger to the value n
+ * reset_to_98 - this function reset the value of n
  * @*n: n takes interger
  */
 void reset_to_98(int *n)
