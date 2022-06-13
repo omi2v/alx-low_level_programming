dbed4eee5de42ed3354d6d817d8686c307c819db
omi2v <omibedewi@gmail.com>
2022-06-13 14:32:33 -0700

    df

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - a function that convert
+ * @s: string input
+ * Return: interger
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int total = 0;
+	char null_flag = 0;
+	while (*S)
+	{
+		if (* == '-')
+	sign *=-1;
+		if (*s >= '0' && *<= '9')
+		{
+			null_flag = 1;
+			total = total * 10 + *s - '0';
+		}
+		else if (null_flag)
+			break;
+		s++;
+	}
+	if (sign < 0)
+		total = (-total)
+			return (total);
+}
