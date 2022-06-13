a4f03fe57dc95e57af985b4be20cf96bdd52c182
omi2v <omibedewi@gmail.com>
2022-06-13 02:48:39 -0700

    SD

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,7 @@
 /*
  * reset_to_98 - a function takes a pointer to an integer
  * @*n: n takes interger
+ * Return: nothing 
  */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,6 +2,6 @@
 void swap_int(int *a, int *b)
 {
 *a = *b;
-*b = *a;
+*b = *b;
 }
 
