49d53245077e6f1b4ca760e915dd7666cc089b66
omi2v <omibedewi@gmail.com>
2022-06-13 06:02:48 -0700

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,12 @@
 #include "main.h"
+/**
+ * _strlen - return the length
+ * @s: declare string
+ * Return: the length 
+ */
 int _strlen(char *s)
 {
-int length = 0 ;
+int length = 0;
 while (s[length] != '\0')
 	length++;
 return (length);
