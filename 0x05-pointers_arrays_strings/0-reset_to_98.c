e261f468aa9abc6ed160bac9713b0c9fc0c3c639
omi2v <omibedewi@gmail.com>
2022-06-13 03:05:17 -0700

    sd

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /*
  * reset_to_98 - a description that update the value of pointer
+ * Description: update the value of pointer
  * @*n: n takes interger
  * Return: nothing
  */
