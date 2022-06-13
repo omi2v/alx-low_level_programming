97b04ba001e5ca23be18fd8d2f754f34ef72c15f
omi2v <omibedewi@gmail.com>
2022-06-13 12:55:46 -0700

    sd

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,10 @@
 #include "main.h"
 void print_rev(char *s)
 {
-	int len = strlen(s);
-	int i = 0, j = 0;
-	char temp[100];
-	for  (i = 0; i < len; i++)
-	{
-		for (j = 0; i < len; j++, i++)
-		{
-			if (s[i] == ' ' || s[i] '.')
-				break;
-			temp[j] = s[i];
-		}
-	}
+	int len = 0;
+	while (s[len] != '\0')
+		len++;
+	while (len)
+		_putchar(s[--len]);
+	_putchar('\n');
 }
