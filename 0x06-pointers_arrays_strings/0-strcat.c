ab02da8ee4ef6e513eed0ba0ee9a0e37882fa086
omi2v <omibedewi@gmail.com>
2022-06-15 00:43:24 -0700

    concatnet

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * *_strcat - description of the main function
+ * @dest: the first string
+ * @src: the second string
+ * Return: always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *result = malloc(strlen(dest) + strlen(src) + 1;
+			strcpy(result, dest);
+			strcpy(result, src);
+			return result;
+			}
