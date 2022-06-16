56896c190c7ccd4adb2d36ce4c0303a3cfd0a146
omi2v <omibedewi@gmail.com>
2022-06-16 01:46:57 -0700

    strcop

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main."
 #include <string.h>
 /**
- **_strncpy - main descriptioon
+ *_strncpy - main descriptioon
  *@dest: char
  *@src: srcc char
  *@n: integer
@@ -9,9 +9,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	for (n = 0; dest[n] != '\0'; n++)
+	int i = 0;
+	while (src[i] != '\0' && i < n)
 	{
-		src[n] = dest[n];
+		dest[i] = src [i];
+		i++;
 	}
-	src[n] = '\0';
+	for (; i < n; i++)
+	{
+	dest[i] != '\0';
+	}
+	return (dest);
 }
