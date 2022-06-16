85750de1f363b12f5053a859b01b97be4ca7c3f7
omi2v <omibedewi@gmail.com>
2022-06-16 02:19:10 -0700

    strcon

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,14 +10,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	while (src[i] != '\0' && i < n)
+while (src[i] != '\0' && i < n)
 	{
-		dest[i] = src [i];
+dest[i] = src[i];
 		i++;
 	}
 	for (; i < n; i++)
 	{
-	dest[i] != '\0';
+	dest[i] = '\0';
 	}
 	return (dest);
 }
