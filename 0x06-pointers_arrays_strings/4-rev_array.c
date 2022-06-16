c8b9a23cfe3f8ba0c030a11683e5d22acae4f305
omi2v <omibedewi@gmail.com>
2022-06-16 02:36:04 -0700

    rev

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *reverse_array - main funcction
+ *@a: integer a
+ *@n: integer n
+ *Return: always 0
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int um;
+	while (i < n--)
+	{
+		um = a[i];
+		a[i++] = a [n];
+		a[n] = um;
+	}
+}
