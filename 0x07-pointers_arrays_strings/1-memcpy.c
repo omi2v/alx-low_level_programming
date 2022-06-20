d46e6ba6ac3823f020ea9753d02ec678f1f645c2
omi2v <omibedewi@gmail.com>
2022-06-20 03:48:42 -0700

    copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,8 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-	for (i = 0; i < n; i++)
-	{
+	unsigned int i = 0;
+for (; i < n; i++)
 		dest[i] = src[i];
-	}
-	return (dest)
+	return (dest);
 }
