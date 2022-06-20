972373e49fb0d527a444aee9979418066239d4a5
omi2v <omibedewi@gmail.com>
2022-06-20 13:11:01 -0700

    sds

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagsums - to print something
+ * @a: integer value
+ * @size: the szeof the thin g
+ * Return: no
+ */
+void print_diagsums(int *a, int size)
+{
+int i = 0;
+int j = size - 1;
+int sum1 = 0;
+int sum2 = 0;
+while (i <= (size * size))
+{
+sum1 = sum1 + a[i];
+i = i + size + 1;
+}
+while (j < (size * size - 1))
+{
+sum2 = sum2 + a[j];
+j = j + size - 1;
+}
+printf("%d, %d\n", sum1, sum2)
+}
