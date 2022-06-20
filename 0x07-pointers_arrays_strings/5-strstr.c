475fe02545d6ce71485cccc688cb403ac81c9a9b
omi2v <omibedewi@gmail.com>
2022-06-20 12:46:03 -0700

    nice

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,20 +7,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-
-    /* A loop to slide pat[] one by one */
-    for (int i = 0; i <= N - M; i++) {
-        int j;
-
-        /* For current index i, check for
- pattern match */
-        for (j = 0; j < M; j++)
-            if (needle[i + j] != haystack[j])
-                break;
-
-        if (j == M)
-            return i;
-}
+	int i;
+	int s = 0;
+	while (needle[s] != '\0';
+			s++;
+			while (*haystack)
+			{
+			for (i = 0; needle[i]; i++)
+			{
+			if (haystack[i] != needle[i])
+			break;
+			}
+			if (i != s)
+			haystack++;
+			else
+			return (haystack);
+			}
 return (NULL);
 }
 
