ce3a27e0fcd1af827b08f610071b4b155519bd7a
omi2v <omibedewi@gmail.com>
2022-06-20 03:31:48 -0700

    fill memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/*_memset - fill the memory
+/**
+ * _memset - fill the memory
  * @s: the string
  * @b: the other string
  * @n: the other integer
@@ -8,7 +9,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	for (i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
