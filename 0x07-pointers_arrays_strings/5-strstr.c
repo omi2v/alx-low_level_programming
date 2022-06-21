422ae2486d5b1d49d30117d21831da3f94b33b2a
omi2v <omibedewi@gmail.com>
2022-06-21 04:13:49 -0700

    dfd

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "main.h"
 /**
  * _strstr - function for to do
@@ -7,22 +8,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-	int s = 0;
-	while (needle[s] != '\0')
-			s++;
-			while (*haystack)
-			{
-			for (i = 0; needle[i]; i++)
-			{
-			if (haystack[i] != needle[i])
-			break;
-			}
-			if (i != s)
-			haystack++;
-			else
-			return (haystack);
-			}
+int i;
+int s = 0;
+while (needle[s] != '\0')
+s++;
+while (*haystack)
+{
+for (i = 0; needle[i]; i++)
+{
+if (haystack[i] != needle[i])
+break;
+}
+if (i != s)
+haystack++;
+else
+return (haystack);
+}
 return (NULL);
 }
 
