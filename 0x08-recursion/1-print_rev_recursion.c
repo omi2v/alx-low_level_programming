b926db6caac439c5aa5d010347938f332d682b55
omi2v <omibedewi@gmail.com>
2022-06-22 03:45:02 -0700

    sd

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ *_print_rev_recursion - function to reverse string
+ *@s: the string
+ *Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+    static int i, len, temp;
+    len = strlen(s); // use strlen() to get the length of str string
+
+
+    if (i < len/2){
+        // temp variable use to temporary hold the string
+        temp = s[i];
+        s[i] = s[len - i - 1];
+        s[len - i - 1] = temp;
+        i++;
+        _print_rev_recursion(s); // recusively calls the revstr() function
+    }
+}
