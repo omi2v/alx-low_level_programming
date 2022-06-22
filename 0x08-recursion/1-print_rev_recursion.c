07ccdae893738a40cdefbbfa0543b10e7959607d
omi2v <omibedewi@gmail.com>
2022-06-22 03:49:23 -0700

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,16 +6,7 @@
  */
 void _print_rev_recursion(char *s)
 {
-    static int i, len, temp;
-    len = strlen(s); // use strlen() to get the length of str string
-
-
-    if (i < len/2){
-        // temp variable use to temporary hold the string
-        temp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = temp;
-        i++;
-        _print_rev_recursion(s); // recusively calls the revstr() function
-    }
+	if (*s != '\0')
+		_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
