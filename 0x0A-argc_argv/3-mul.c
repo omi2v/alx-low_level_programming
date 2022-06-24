cc9e877e725a6ce836111322f896ab390f7ac7c8
omi2v <omibedewi@gmail.com>
2022-06-24 07:40:37 -0700

    nice

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main -  the main functio
+ *@argc: the count arg
+ *@argv: the vector
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int p, j, mul;
+if (argc != 3)
+{
+printf("Error\n");
+return (-1);
+}
+p = atoi(argv[1]);
+j = atoi(argv[2]);
+mul = p *j;
+printf("mul=%d", mul);
+return (0);
+}
+
