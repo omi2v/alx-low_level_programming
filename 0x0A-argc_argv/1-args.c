53c70d5ad7e062cce228d30f4eb8b7e457dc8dbf
omi2v <omibedewi@gmail.com>
2022-06-24 06:52:20 -0700

    pased

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - the function
+ *@argc: the count argument
+ *@argv: the vector srgumnent
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
+	return (0);
+}
