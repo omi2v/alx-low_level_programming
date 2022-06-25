17923f071109503d25341dc7178e746706034e59
omi2v <omibedewi@gmail.com>
2022-06-25 05:43:26 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  *main -  the main functio
  *@argc: the count arg
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include "main.h"
-#include <stdio..h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 /**
  *main - main function
  *@argc: arg to count
