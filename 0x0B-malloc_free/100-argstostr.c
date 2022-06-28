2392eb51cc734187abdca70cf6289fdb988215ff
omi2v <omibedewi@gmail.com>
2022-06-28 12:22:48 -0700

    arg

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *argstostr - function used to concate
+ *@ac: the first integer
+ *@av: the first char
+ *Return: something
+ */
+char *argstostr(int ac, char **av)
+{
+	char *ar;
+	int i;
+	int j;
+	int k = 0;
+	int n;
+	int m;
+	int counter = 0;
+
+	if (ac == 0)
+		return (NULL);
+	if (av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			counter++;
+
+		counter++;
+	}
+	counter++; /* to account for \n that needs to be added later */
+	ar = malloc(sizeof(char) * counter);
+	if (ar == NULL)
+		return (NULL);
+
+	for (n = 0; n < ac; n++)
+	{
+		for (m = 0; av[n][m] != '\0'; m++)
+		{
+			ar[k] = av[n][m];
+			k++;
+		}
+		ar[k] = '\n';
+		k++;
+	}
+	ar[k] = '\0';
+	return (ar);
+}
