727ccbf941b23c56e95b7d92171d367c7828b770
omi2v <omibedewi@gmail.com>
2022-06-28 12:41:32 -0700

    str

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *strtow - function to str
+ *@str: the first char
+ *Return: something
+ */
+char **strtow(char *str)
+{
+	char *s;
+unsigned int t;
+	if (str == NULL || str == "")
+		return (NULL);
+	if (str == NULL)
+		return (0);
+	for (t = 0; str[s] != NULL; t++)
+	{
+		str[s] = "";
+}
+	return (NULL);
+	}
