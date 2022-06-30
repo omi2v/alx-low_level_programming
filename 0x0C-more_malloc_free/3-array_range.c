8f9fb8946ccb0054eca5b79ba290236d1b782607
omi2v <omibedewi@gmail.com>
2022-06-30 04:23:37 -0700

    arry conv

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - array fun
+ * @min: the first min
+ * @max: the second maxxxxxx
+ * Return: something
+ */
+int *array_range(int min, int max)
+{
+int p;
+int q = 0;
+char *ar;
+if (min > max)
+return (NULL);
+ar = malloc(sizeof(int) * (max - min + 1));
+if (ar == NULL)
+return (NULL);
+for (p = min; p <= max; p++)
+{
+ar[q] = p;
+j++;
+}
+return (ar);
+}
+
