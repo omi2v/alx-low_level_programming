27b9694eabdf3963050aee65d1904e13c369dae9
omi2v <omibedewi@gmail.com>
2022-06-30 04:09:33 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,14 +8,14 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int p;
-	char *ar;
-	if (size == 0 || nmemb == 0)
-		return (NULL);
-	ar = malloc(size * nmemb);
-			if (ar == NULL)
-				return (NULL);
-	for (p = 0; p < (size * nmemb); p++)
-		ar[p] = 0;
-	return (ar);
+unsigned int p;
+char *ar;
+if (size == 0 || nmemb == 0)
+return (NULL);
+if (ar == NULL)
+return (NULL);
+ar = malloc(size * nmemb);
+for (p = 0; p < (size * nmemb); p++)
+ar[p] = 0;
+return (ar);
 }
