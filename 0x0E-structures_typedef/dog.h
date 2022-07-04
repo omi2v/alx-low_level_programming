01ed479195bc74b943d42efafc270d45e9c9c12a
omi2v <omibedewi@gmail.com>
2022-07-04 07:19:59 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef _d_home_
+#define _d_home_
+/**
+ * struct dog - description
+ * @name: dog name
+ * @age: dg age
+ * @owner: dog owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
