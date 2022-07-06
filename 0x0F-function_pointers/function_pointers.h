9f8e5bb5123036fc8326547d3b7d5417925576b6
omi2v <omibedewi@gmail.com>
2022-07-06 06:21:33 -0700

    name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,2 +1,6 @@
+#include <stdio.h>
+#ifndef _FUNCTION_POINTER_H_
+#define _FUNCTION_POINTER_H_
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
