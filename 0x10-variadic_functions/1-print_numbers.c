79c2907fe16302e0c896455cf84b110492c71ea5
omi2v <omibedewi@gmail.com>
2022-07-07 02:38:33 -0700

    num

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+/**
+ * print_numbers - function to print numbers
+ * @separator: a character used to separate
+ * @n: the constant integer
+ * Return: something
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list list;
+unsigned int i;
+va_start(list, n);
+for (i = 0; i < n; i++)
+{
+printf("%i", va_arg(valist, int));
+if (separator != NULL && i < n - 1)
+printf("%s", separator);
+}
+printf("\n");
+va_end(list);
+}
