46ebd03f355cb87b202dc984e483c63343c35d92
omi2v <omibedewi@gmail.com>
2022-07-07 02:13:38 -0700

    sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - functioon to sum all
+ * @n: the n times integer
+ * Return: 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int x, sum = 0;
+	if (n == 0)
+		return (0);
+	va_list(valist, n)
+	for (x = 0; x < n; x++)
+		sum += va_arg(valist, int);
+	va_end(valist);
+		return (sum);
+}
+
