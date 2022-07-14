ad13276c9c89b733f304305acc5a3298b353bd61
omi2v <omibedewi@gmail.com>
2022-07-14 14:22:52 -0700

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,9 +11,9 @@ void free_list(list_t *head)
 list_t *temp;
 while (head != NULL)
 {
-head = head->next;
-free(temp->str);
-free(temp);
-temp = head;
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
 }
 }
