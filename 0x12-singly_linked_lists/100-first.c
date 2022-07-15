1d1f7ae4c49ae3cc66578e2ca4b062e46cba51af
omi2v <omibedewi@gmail.com>
2022-07-15 03:59:41 -0700

    cool

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "lists.h"
 /**
  * main - main function
  * Return: nothing
  */
-int main (void)
+list_t *add_nod(list_t **head)
 {
+	while (head != NULL)
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
-
