e0ea3a94083c7ece52559e81af66ce7ffabf9e24
omi2v <omibedewi@gmail.com>
2022-07-15 03:44:10 -0700

    print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: nothing
+ */
+int main (void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
