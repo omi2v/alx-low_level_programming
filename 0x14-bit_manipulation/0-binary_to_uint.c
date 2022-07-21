10977fc62fca900595e3fa229a716df568d57127
omi2v <omibedewi@gmail.com>
2022-07-21 14:23:17 -0700

    sds

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+#include <string.h>
 /**
  * binary_to_uint - convert binary
  * @b: the b character
@@ -7,33 +9,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
-	unsigned int n = 0;
-	int power = 0;
-
-	if (b == "\0")
-		return (0);
-
-	while (b[i + 1])
-		i++;
-
-	while (i >= 0)
-	{
-		if (b[i] == '0')
-		{
-			i--;
-			power++;
-		}
-		else if (b[i] == '1')
-		{
-			n += (1 << power);
-			i--;
-			power++;
-		}
-		else
-			return (0);
-	}
-	return (n);
-
-
+    unsigned int k = 2;
+    unsigned int i;
+    unsigned int c;
+    unsigned int len;
+    if (b == NULL)
+        return (0);
+    len = strlen(b);
+    for (c = len; c > 0; c--)
+    {
+        if (b[c] != 48 || b[c] != 49)
+            return (0);
+        if (b[c] == '1')
+        {
+            i += atoi(b) * k;
+        }
+        k *= 2;
+    }
+    return (i);
 }
