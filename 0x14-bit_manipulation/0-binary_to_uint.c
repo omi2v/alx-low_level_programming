778687562f42844e5093a3c70a3f1537a3261ecc
omi2v <omibedewi@gmail.com>
2022-07-21 14:13:41 -0700

    sD

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,31 +3,37 @@
  * binary_to_uint - convert binary
  * @b: the b character
  * Return: converted number
+ * string @b that is not 0 or 1, or @b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int i = 0;
-unsigned int n = 0;
-unsigned int power = 0;
-if (b = '\0')
-return (0);
-while (b[i + 1])
-i++;
-while (i >= 0)
-{
-if (b[i] == '0')
-{
-i--;
-power++;
-}
-else if (b[i] == '1')
-{
-n += (1 << power);
-i--;
-power++;
-}
-else
-return (0);
-}
-return (n);
+	int i = 0;
+	unsigned int n = 0;
+	int power = 0;
+
+	if (b == "\0")
+		return (0);
+
+	while (b[i + 1])
+		i++;
+
+	while (i >= 0)
+	{
+		if (b[i] == '0')
+		{
+			i--;
+			power++;
+		}
+		else if (b[i] == '1')
+		{
+			n += (1 << power);
+			i--;
+			power++;
+		}
+		else
+			return (0);
+	}
+	return (n);
+
+
 }
