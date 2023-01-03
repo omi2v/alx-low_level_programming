4c3f34b948397752614c6d443b148b8584e940e3
omi2v <omibedewi@gmail.com>
2023-01-03 02:31:49 -0800

    line

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,16 +9,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-int found = 0;
-int i;
-for (i = 0; i < size; i++)
-{
-if (array[i] == value)
-return (value);
-found = 1;
-}
-if (array == NULL)
-{
-return (-1);
-}
+size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
 }
